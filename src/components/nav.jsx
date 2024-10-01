f73369216297b3357e6031c94d465ba3e163622b
shibsankar-stu <dasshibsankar94732@gmail.com>
2024-10-01 21:09:21 +0530

    add photo

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,7 +3,7 @@ function Mynav() {
         <nav class="navbar bg-body-tertiary" className=" Mynav">
             <div class="container">
                 <a class="navbar-brand" href="#">
-                    <img src="/src/assets/logo.webp" alt="Bootstrap" width="40" height="37" /> VirtualR
+                    <img src="./src/assets/logo.webp" alt="Bootstrap" width="40" height="37" /> VirtualR
                 </a>
             </div>
             <div className="listItem">
@@ -11,7 +11,7 @@ function Mynav() {
                     <a href="#">Home</a>
                     <a href="features.jsx"><li>Freatures</li></a>
                     <a href="workflow.jsx"><li>Workflow</li></a>
-                   <a href="pricung.jsx"> <li>Pricing</li></a>
+                   <a href="pricung.jsx"><li>Pricing</li></a>
                    <a href="#"> <li>Testimonial</li></a>
                 </ul>
             </div>
